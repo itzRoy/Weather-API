5b190634e087bbd03fa2f19996827629435845bc
Roy <royissa3@gmail.com>
2021-12-16 18:56:18 +0200

    Weather API: HTML/CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from "react";
-import Search from "./components/Search";
+// import Search from "./components/Search";
 
-import SayHi, { SayHello } from "./components/WeatherItem";
-import fakeWeatherData from "./fakeWeatherData.json";
+// import SayHi, { SayHello } from "./components/WeatherItem";
+// import fakeWeatherData from "./fakeWeatherData.json";
 
 import "./App.css";
+
+//images
+import clear from './img/weather-icons/clear.svg';
+import cloudy from './img/weather-icons/cloudy.svg';
+import drizzle from './img/weather-icons/drizzle.svg';
+import fog from './img/weather-icons/fog.svg';
+import mostlycloudy from './img/weather-icons/mostlycloudy.svg';
+import partlycloudy from './img/weather-icons/partlycloudy.svg';
+import rain from './img/weather-icons/rain.svg';
+import snow from './img/weather-icons/snow.svg';
+import storm from './img/weather-icons/storm.svg';
+
 
 class App extends Component {
   constructor(props) {
@@ -21,10 +33,71 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <SayHi />
+        <div className="siteWrapper">
+          <header>
+            <form>
+            <input placeholder={"City Name"}/>
+            <button type="submit">FIND WEATHER</button>
+            </form>
+          </header>
+          <main className="card">
+          <img src={partlycloudy} alt="partlycloudy"/>
+          <p>overcast clouds</p>
+          <div><strong>Temperature </strong><span> 10° to 11℃</span></div>
+          <div><bold>Humidity </bold><small> 78% </small><bold> Pressure </bold><small> 100848</small></div>
+          </main>
+          <section>
+            <div className="cardsContainer">
+              <div className="cards">
+                <p>03:00</p>
+                <img src={partlycloudy} alt="partlycloudy"/>
+                <p>8℃</p>
+              </div>
+
+              <div className="cards">
+                <p>06:00</p>
+                <img src={partlycloudy} alt="partlycloudy"/>
+                <p>9℃</p>
+              </div>
+
+              <div className="cards">
+                <p>09:00</p>
+                <img src={clear} alt="clear"/>
+                <p>14℃</p>
+              </div>
+
+              <div className="cards">
+                <p>12:00</p>
+                <img src={clear} alt="clear"/>
+                <p>17℃</p>
+              </div>
+
+              <div className="cards">
+                <p>015:00</p>
+                <img src={clear} alt="clear"/>
+                <p>18℃</p>
+              </div>
+
+              <div className="cards">
+                <p>18:00</p>
+                <img src={clear} alt="clear"/>
+                <p>16℃</p>
+              </div>
+
+              <div className="cards">
+                <p>21:00</p>
+                <img src={partlycloudy} alt="partlycloudy"/>
+                <p>13℃</p>
+              </div>
+            </div>
+
+          </section>
+        </div>
+        {/* <SayHi />
         <SayHello color="black" name={this.state.name} />
-        <Search handleInput={this.handleInputChange} />
+        <Search handleInput={this.handleInputChange} /> */}
       </div>
+      
     );
   }
 }
